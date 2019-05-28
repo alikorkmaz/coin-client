974904a10d855c3e3cbcebbd5d3654db9e7aedeb
Ali Korkmaz <ali@Alis-MacBook-Pro.local>
2019-05-28 19:56:17 +0300

    Updates

---
+++ precache-manifest.3c7da76f15aae96b476ed276b212b9a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a335f86b3a9ec0e7b7415843da07981",
+    "url": "/coin-client/index.html"
+  },
+  {
+    "revision": "6f97e62b774b0f7bce0e",
+    "url": "/coin-client/static/css/main.f08b232e.chunk.css"
+  },
+  {
+    "revision": "bf8f57def0e1b6b79f10",
+    "url": "/coin-client/static/js/2.05c5614a.chunk.js"
+  },
+  {
+    "revision": "6f97e62b774b0f7bce0e",
+    "url": "/coin-client/static/js/main.71958745.chunk.js"
+  },
+  {
+    "revision": "5ad3f08533f21ad8e339",
+    "url": "/coin-client/static/js/runtime~main.f140d4f5.js"
+  }
+]);
